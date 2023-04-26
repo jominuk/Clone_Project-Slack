9b1d1ccb4ea8c9a6ac724350590a6dee19724746
jominuk <jominuk1025@naver.com>
2023-04-27 08:57:49 +0900

    custom hook

--- setting/ts/pages/SignUp/index.tsx
+++ setting/ts/pages/SignUp/index.tsx
@@ -1,22 +1,15 @@
 import React, { useCallback, useState } from 'react';
 import { Error, Form, Label, Input, LinkContainer, Button, Header } from './styles';
 import { Link } from 'react-router-dom';
+import useInput from '@pages/hooks/useinput';
 
 const SignUp = () => {
-  const [email, setEmail] = useState('');
-  const [nickname, setNickname] = useState('');
+  const [email, onChangeEmail] = useInput('');
+  const [nickname, onChangeNickname] = useInput('');
   const [password, setPassword] = useState('');
   const [passwordCheck, setPasswordCheck] = useState('');
 
   const [mismatchError, setMismatchError] = useState(false);
-
-  const onChangeEmail = useCallback((e) => {
-    setEmail(e.target.value);
-  }, []);
-
-  const onChangeNickname = useCallback((e) => {
-    setNickname(e.target.value);
-  }, []);
 
   const onChangePassword = useCallback(
     (e) => {
---
+++ setting/ts/pages/hooks/useinput.ts
@@ -0,0 +1,42 @@
+// =====================지금은 리턴부분을 변수로 빼서 타입을 정함
+import { Dispatch, SetStateAction, useCallback, useState } from 'react';
+
+type ReturnType<T = any> = [T, (e: any) => void, Dispatch<SetStateAction<T>>];
+
+const useInput = <T = any>(initialData: T): ReturnType<T> => {
+  // 매개변수는 추론이 불가능해 type을 지정하고 그 뒤에 있는 return 값에도 type을 지정함
+  // T의미는 제너릭이라는 변수를 만든다. 의미로 any와 같은 의미이다.
+  // any로 해서 안좋은 점이 initialDate의 타입이 string라면 바로 뒤에 있는 T가 자동으로 string로 바뀌지 않는다.
+  // 커스텀 훅 : 7:39초
+  // T는 value와 같고, (e: any) => void 는 handler, Dispatch<SetStateAction<T>> 는 setValue 와 같은 의미이다.
+  const [value, setValue] = useState(initialData);
+
+  const handler = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+  return [value, handler, setValue];
+};
+
+export default useInput;
+
+// ============================지금 이곳이 먼저 작성했던 부분
+
+// import { Dispatch, SetStateAction, useCallback, useState } from 'react';
+
+// const useInput = <T = any>(initialData: T): [T, (e: any) => void, Dispatch<SetStateAction<T>>] => {
+//   // 매개변수는 추론이 불가능해 type을 지정하고 그 뒤에 있는 return 값에도 type을 지정함
+//   // T의미는 제너릭이라는 변수를 만든다. 의미로 any와 같은 의미이다.
+//   // any로 해서 안좋은 점이 initialDate의 타입이 string라면 바로 뒤에 있는 T가 자동으로 string로 바뀌지 않는다.
+//   // 커스텀 훅 : 7:39초
+//   // T는 value와 같고, (e: any) => void 는 handler, Dispatch<SetStateAction<T>> 는 setValue 와 같은 의미이다.
+//   const [value, setValue] = useState(initialData);
+
+//   const handler = useCallback((e) => {
+//     setValue(e.target.value);
+//   }, []);
+//   return [value, handler, setValue];
+// };
+
+// export default useInput;
+
+// //매개변수에는 추론이 불가능해서 type을 선언해야한다.
