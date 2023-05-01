2b4cd808fcae2ca1ee0ca96a4cd049066bc83de1
jominuk <jominuk1025@naver.com>
2023-05-01 16:39:01 +0900

    modal

--- setting/ts/layouts/Workspace/index.tsx
+++ setting/ts/layouts/Workspace/index.tsx
@@ -5,8 +5,11 @@ import fetcher from '@utils/fetcher';
 import axios from 'axios';
 import { Redirect, Switch, Route } from 'react-router';
 import loadable from '@loadable/component';
+import { IUser } from '@typings/db';
+import { toast } from 'react-toastify';
 
 import {
+  AddButton,
   Channels,
   Chats,
   Header,
@@ -15,19 +18,28 @@ import {
   ProfileImg,
   ProfileModal,
   RightMenu,
+  WorkspaceButton,
   WorkspaceName,
   WorkspaceWrapper,
   Workspaces,
 } from './styles';
 import Menu from '@components/Menu';
+import { Link } from 'react-router-dom';
+import Modal from '@components/Modal';
+import { Button, Input, Label } from '@pages/SignUp/styles';
+import useInput from '@hooks/useinput';
 
 const Channel = loadable(() => import('@pages/Channel'));
 const DirectMessage = loadable(() => import('@pages/DirectMessage'));
 
-const Workspace: FC = ({ children }) => {
-  const { data, mutate } = useSWR('http://localhost:3095/api/users', fetcher);
+const Workspace: VFC = () => {
+  const { data: userData, mutate } = useSWR<IUser | false>('http://localhost:3095/api/users', fetcher);
 
   const [showUserMenu, setShowUserMenu] = useState(false);
+  const [showCreateWorkspaceModal, setShowCreateWorkspaceModal] = useState(false);
+  const [newWorkspace, onChangeNewWorkspace, setNewWorkpsace] = useInput('');
+  const [newUrl, onChangeNewUrl, setNewUrl] = useInput('');
+  const [showWorkspaceModal, setShowWorkspaceModal] = useState(false);
 
   const onLogout = useCallback(() => {
     axios
@@ -39,11 +51,55 @@ const Workspace: FC = ({ children }) => {
       });
   }, []);
 
-  const onClickUserProfile = useCallback(() => {
+  const onClickUserProfile = useCallback((e) => {
+    e.stopPropagation();
     setShowUserMenu((prev) => !prev);
   }, []);
 
-  if (!data) {
+  const onClickCreateWorkspace = useCallback(() => {
+    setShowCreateWorkspaceModal(true);
+  }, []);
+
+  const onCreateWorkspace = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (!newWorkspace || !newWorkspace.trim()) return;
+      if (!newUrl || !newUrl.trim()) return;
+      // trim을 넣어야 띄어쓰기가 막을 수 있음
+      axios
+        .post(
+          'http://localhost:3095/api/workspaces',
+          {
+            workspace: newWorkspace,
+            url: newUrl,
+          },
+          {
+            withCredentials: true,
+          },
+        )
+        .then(() => {
+          mutate(userData, true);
+          setShowCreateWorkspaceModal(false);
+          setNewWorkpsace('');
+          setNewUrl('');
+        })
+        .catch((error) => {
+          console.log(error);
+          toast.error(error.response?.data, { position: 'bottom-center' });
+        });
+    },
+    [newWorkspace, newUrl],
+  );
+
+  const onCloseModal = useCallback(() => {
+    setShowCreateWorkspaceModal(false);
+  }, []);
+
+  const toggleWorkspaceModal = useCallback(() => {
+    setShowWorkspaceModal((prev) => !prev);
+  }, []);
+
+  if (!userData) {
     return <Redirect to="/login" />;
   }
 
@@ -52,30 +108,39 @@ const Workspace: FC = ({ children }) => {
       <Header>
         <RightMenu>
           <span onClick={onClickUserProfile}>
-            <ProfileImg src={gravatar.url(data.nickname, { s: '28px', d: 'retro ' })} alt={data.nickname} />
-
+            <ProfileImg src={gravatar.url(userData.email, { s: '28px', d: 'retro' })} alt={userData.nickname} />
             {showUserMenu && (
               <Menu style={{ right: 0, top: 38 }} show={showUserMenu} onCloseModal={onClickUserProfile}>
                 <ProfileModal>
-                  <img src={gravatar.url(data.nickname, { s: '36px', d: 'retro ' })} alt={data.nickname} />
-
+                  <img src={gravatar.url(userData.nickname, { s: '36px', d: 'retro' })} alt={userData.nickname} />
                   <div>
-                    <span id="profile-name"> {data.nickname} </span>
-                    <span id="profile-active"> Active </span>
+                    <span id="profile-name">{userData.nickname}</span>
+                    <span id="profile-active">Active</span>
                   </div>
                 </ProfileModal>
-
-                <LogOutButton onClick={onLogout}> 로그아웃 </LogOutButton>
+                <LogOutButton onClick={onLogout}>로그아웃</LogOutButton>
               </Menu>
             )}
           </span>
         </RightMenu>
       </Header>
       <WorkspaceWrapper>
-        <Workspaces> test </Workspaces>
+        <Workspaces>
+          {userData?.Workspaces?.map((ws) => {
+            return (
+              <Link key={ws.id} to={`/workspace/${123}/channel/일반`}>
+                <WorkspaceButton>{ws.name.slice(0, 1).toUpperCase()}</WorkspaceButton>
+              </Link>
+            );
+          })}
+          <AddButton onClick={onClickCreateWorkspace}>+</AddButton>
+        </Workspaces>
         <Channels>
-          <WorkspaceName> sleact </WorkspaceName>
-          <MenuScroll> menuscroll </MenuScroll>
+          <WorkspaceName onClick={toggleWorkspaceModal}>
+            슬랙
+            {/* {userData?.Workspaces?.find()} */}
+          </WorkspaceName>
+          <MenuScroll>menu scroll</MenuScroll>
         </Channels>
         <Chats>
           <Switch>
@@ -83,7 +148,21 @@ const Workspace: FC = ({ children }) => {
             <Route path="/workspace/dm" component={DirectMessage} />
           </Switch>
         </Chats>
+        {/* Input이 들어있으면 별도의 컴포넌트로 빼는 것을 추천(input에 글자를 입력할 때마다 여기 있는 함수들이 다 리렌더링 되기 때문에 비효율적) */}
       </WorkspaceWrapper>
+      <Modal show={showCreateWorkspaceModal} onCloseModal={onCloseModal}>
+        <form onSubmit={onCreateWorkspace}>
+          <Label id="workspace-label">
+            <span>워크스페이스 이름</span>
+            <Input id="workspace" value={newWorkspace} onChange={onChangeNewWorkspace} />
+          </Label>
+          <Label id="workspace-url-label">
+            <span>워크스페이스 이름</span>
+            <Input id="workspace" value={newUrl} onChange={onChangeNewUrl} />
+          </Label>
+          <Button type="submit">생성하기</Button>
+        </form>
+      </Modal>
     </div>
   );
 };
