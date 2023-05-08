968166c95426e6ba19f0a757d7310a004b02405b
jominuk <jominuk1025@naver.com>
2023-05-08 15:40:07 +0900

    추가 입력

--- setting/ts/components/ChatList/index.tsx
+++ setting/ts/components/ChatList/index.tsx
@@ -21,6 +21,8 @@ const ChatList = forwardRef<Scrollbars, Props>(({ chatSections, setSize, isEmpty
         if (current) {
           current.scrollTop(current.getScrollHeight() - values.scrollHeight);
           console.log(current?.getScrollHeight(), values.scrollHeight);
+          // 콘솔에 입력되는 숫자에서 앞쪽에는 전체 높이를 나타내주고 있고
+          // 두 쪽 숫자는 현재 스크롤바의 위치를 나태내 주고 있음
         }
       });
     }
